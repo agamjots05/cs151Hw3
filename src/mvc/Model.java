7dfdb64566f95f247d25a0c756f1a3a0a2852c95
canhuihuang <huangcanhui121@gmail.com>
2025-03-14 00:12:37 -0700

    Modified Model setters/getters to public

--- src/mvc/Model.java
+++ src/mvc/Model.java
@@ -6,19 +6,19 @@ public abstract class Model extends Publisher implements Serializable {
     private String fileName = null;
     private Boolean unsavedChanges = false;
 
-    private String getFileName(){
+    public String getFileName(){
         return this.fileName;
     }
 
-    private void setFileName(String newFileName){
+    public void setFileName(String newFileName){
         this.fileName = newFileName;
     }
 
-    private Boolean getUnsavedChanges(){
+    public Boolean getUnsavedChanges(){
         return this.unsavedChanges;
     }
 
-    private void setUnsavedChanges(Boolean unsavedChanges) {
+    public void setUnsavedChanges(Boolean unsavedChanges) {
         this.unsavedChanges = unsavedChanges;
     }
 

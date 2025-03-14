09bee50ade2db2eeac060f156eef148659a4d20f
canhuihuang <huangcanhui121@gmail.com>
2025-03-14 05:00:40 -0700

    set Model methods signatures to protected so they're accesible

--- src/mvc/Model.java
+++ src/mvc/Model.java
@@ -6,23 +6,23 @@ public class Model extends Publisher implements Serializable {
     private String fileName = null;
     private Boolean unsavedChanges = false;
 
-    public String getFileName(){
+    protected String getFileName(){
         return this.fileName;
     }
 
-    public void setFileName(String newFileName){
+    protected void setFileName(String newFileName){
         this.fileName = newFileName;
     }
 
-    public Boolean getUnsavedChanges(){
+    protected Boolean getUnsavedChanges(){
         return this.unsavedChanges;
     }
 
-    public void setUnsavedChanges(Boolean unsavedChanges) {
+    protected void setUnsavedChanges(Boolean unsavedChanges) {
         this.unsavedChanges = unsavedChanges;
     }
 
-    public void changed(){
+    protected void changed(){
         setUnsavedChanges(true);
         notifySubscribers();
     }
